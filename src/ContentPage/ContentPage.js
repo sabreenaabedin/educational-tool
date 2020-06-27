28a6d6b6c87df4f48175934fc1a960da3ef17ea1
breannar888 <67029835+breannar888@users.noreply.github.com>
2020-06-26 19:00:52 -0500

    input data from sampledata.json
    unsure about-
    "content": [
        {"paragraph":"..."}
      , {"paragraph":"..."}
    ]
    -and how to properly reference data from this array.

--- src/ContentPage/ContentPage.js
+++ src/ContentPage/ContentPage.js
@@ -5,7 +5,14 @@ class ContentPage extends Component {
   render() {
     return (
         <div>
-            <p>{"data on this page"}</p>
+            <h2>{"sampledata.title"}</h2>
+            <div>
+              {"sampledata.content"}
+               // is it "sampledata.content.paragraph"/ do i need to specify ".paragraph" or cycle through the array? 
+               <p>{"sampledata.paragraph"}</p>
+               <p>{"sampledata.paragraph"}</p>
+            </div>
+            <h4>{"sampledata.Sources"}</h4>
         </div>
     );
   }
