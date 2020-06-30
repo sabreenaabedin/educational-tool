90c55b9b6cdadce5839654bf8044da84b92912f8
Sabreena <sabreenaabedin@gmail.com>
2020-06-29 20:58:03 -0400

    import instant search styling

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
-// imports from dependencies 
 import React, { Component } from 'react';
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox } from 'react-instantsearch-dom';
-// import { InstantSearch, SearchBox, Hits, Highlight } from 'react-instantsearch-dom';
-
-// internal imports
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 import './App.css';
 import ContentPage from './ContentPage/ContentPage.js';
 // import Results from './Results.js';
 
 const searchClient = algoliasearch('FUG82HNJF7', '79cf8a98d1734a2741a4dfdb669c4762');
 
-// const Hit = ({hit}) =>
-//   <div className="hit">
-//       <div className = "hitName">
-//         {hit.name}
-//       </div>
-//   </div>
+const Hit = ({hit}) =>
+  <div className="hit">
+      <div>
+        {hit.title}
+      </div>
+  </div>
 
-// const Content = () =>
-//   <div className = "content">
-//     <Hits hitComponent = {Hit}/>
-//   </div>
+const Content = () =>
+  <div className = "content">
+    <Hits hitComponent = {Hit}/>
+  </div>
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <div className="App-header">
-            <h1>Basic Guide to BLM Title</h1>
+            <h1>Equity Blueprint</h1>
+            <h3>An Ally's Guide to BLM</h3>
           </div>
-          <div class="searchbar">
-            <InstantSearch searchClient={searchClient} indexName="blm-info">
-              <SearchBox translations = {{placeholder:'Search'}}/>
+          <div>
+            <InstantSearch searchClient={searchClient} indexName="blm-info" >
+              <header class="search-header">
+                <SearchBox translations = {{placeholder:'Search'}}/>
+              </header>
+              
+              <main>
+                <Content />
+              </main>
+              
             </InstantSearch>
           </div>
+          
           
           <ContentPage />
           {/* <Results /> */}
