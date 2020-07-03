42b68cffcb3219bf28acac45bc1bf133f4c6edff
Sabreena <sabreenaabedin@gmail.com>
2020-07-03 17:14:08 -0400

    infinite hits

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+import { InstantSearch, SearchBox, Hits, connectInfiniteHits } from 'react-instantsearch-dom';
 import Button from '@material-ui/core/Button';
 
 import './Search.css';
 
 const searchClient = algoliasearch('FUG82HNJF7', '79cf8a98d1734a2741a4dfdb669c4762');
 
-const Hit = ({hit}) =>
-  <div className="hit">
-    {hit.title}
-    <Button> {"Read More"}</Button>
+
+
+// const Hit = ({hit}, showContentPage) =>
+//   <div className="hit">
+//     {hit.title}
+//     <Button onClick={() => showContentPage}> {"Read More"}</Button>
+//   </div>
+
+
+
+const InfiniteHits = ({
+  hits,
+  hasPrevious,
+  refinePrevious,
+  hasMore,
+  refineNext,
+}) => (
+  <div>
+    <button disabled={!hasPrevious} onClick={refinePrevious}>
+      Show previous
+    </button>
+    <ol>
+      {hits.map(hit => (
+        <div className="hit">
+            {hit.title}
+            <Button onClick={() => showContentPage}> {"Read More"}</Button>
+        </div>
+      ))}
+    </ol>
+    <button disabled={!hasMore} onClick={refineNext}>
+      Show more
+    </button>
   </div>
+);
+
+const CustomInfiniteHits = connectInfiniteHits(InfiniteHits);
 
 class Search extends Component {
+
+    showContentPage() {
+        console.log("here")
+    }
     render() {
         return (
             <InstantSearch searchClient={searchClient} indexName="blm-info" >
@@ -21,7 +56,8 @@ class Search extends Component {
                     <SearchBox translations = {{placeholder:'Search'}}/>
                 </header>
                 <main>
-                    <Hits hitComponent = {Hit} className="content"/>
+                    {/* <InfiniteHits hitComponent = {Hit} className="content"/> */}
+                    <CustomInfiniteHits />
                 </main>
             </InstantSearch>
         );
