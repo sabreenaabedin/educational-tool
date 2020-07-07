3257ea366e7c68ebd6e9f3bc05708d34f294a813
Sabreena <sabreenaabedin@gmail.com>
2020-07-06 20:22:40 -0400

    commented with data structure

--- src/ContentPage/ContentPage.js
+++ src/ContentPage/ContentPage.js
@@ -1,59 +1,73 @@
 import React, { Component } from 'react';
-import data from './sampledata.json';
+import data from '../sampledata.json';
 
 class ContentPage extends Component {
+
+  inputData() {
+    console.log(data[0].content)
+
+    // for( each item in data)
+    //     data(i).content
+    //data[4].content
+    //      data(i). title
+    //      data(i). sources
+
+    // for each item in data(i.content)
+
+    // <h>  data(i).content(j) </h>
+    
+    return ""
+    // sampledata[0].title
+    // sampledata[0].source
+    // sampledata[0].tags
+    // const newCont = data['content'];
+    // const newTitle = data['title'];
+    // const newSrce = data['source'];
+    // const newTags = data['tags'];
+
+    //   //loop through array to get all the data from title
+    //   for (let i = 0; i < newTitle.length; i++){
+
+    //     //create html elements for the title
+    //     const newTitle = document.createElement('h2');
+    //     //put the text content of title into the new elements
+    //     newTitle.textContent = newTitle[i].title;
+    //     }
+
+    // return(<h2>test</h2>)
+
+
+    //   for (let i = 0; i < newCont.length; i++){
+
+    //     const newPara = document.createElement('p');
+    //     const newLink = document.createElement('p');
+
+    //     newPara.textContent = newCont[i].paragraph;
+    //     newLink.textContent = newCont[i].link;
+    //     }
+      
+
+    //   for (let i = 0; i < newSrce.length; i++){
+
+    //     const newPara = document.createElement('p');
+    //     newPara.textContent = newSrce[i].source;
+    //     }
+      
+
+    //   for (let i = 0; i < newTags.length; i++){
+
+    //     const newDiv = document.createElement('div');
+    //     newDiv.textContent = newTags[i].tags;
+    //     }
+
+  }
+
   render() {
+    
     return (
         <div>
-          <p>content here
-            {
-              function inputdata() {
-
-              let newData = data;
-              
-               sampledata[0].content;
-              // sampledata[0].title
-              // sampledata[0].source
-              // sampledata[0].tags
-              const newCont = newData['content'];
-              const newTitle = newData['title'];
-              const newSrce = newData['source'];
-              const newTags = newData['tags'];
-
-                //loop through array to get all the data from title
-	              for (let i = 0; i < newTitle.length; i++){
-
-                  //create html elements for the title
-                  const newTitle = document.createElement('h2');
-                  //put the text content of title into the new elements
-                  newTitle.textContent = newTitle[i].title;
-                  }
-
-
-	              for (let i = 0; i < newCont.length; i++){
-
-                  const newPara = document.createElement('p');
-                  const newLink = document.createElement('p');
-
-                  newPara.textContent = newCont[i].paragraph;
-                  newLink.textContent = newCont[i].link;
-                  }
-                
-
-	              for (let i = 0; i < newSrce.length; i++){
-
-                  const newPara = document.createElement('p');
-                  newPara.textContent = newSrce[i].source;
-                  }
-                
-
-	              for (let i = 0; i < newTags.length; i++){
-
-                  const newDiv = document.createElement('div');
-                  newDiv.textContent = newTags[i].tags;
-                  }
-                }
-            }</p>
+          <p>content here</p>
+          <p>{this.inputData()}</p>
         </div>
     );
   }
